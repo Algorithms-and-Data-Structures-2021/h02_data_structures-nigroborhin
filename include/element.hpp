aff263b5185e70c7d6d63e67769db191b4a69d80
nigroborhin <nigroborgin@yandex.ru>
2021-03-24 12:40:38 +0300

    create method Add

--- include/element.hpp
+++ include/element.hpp
@@ -3,16 +3,16 @@
 namespace itis {
 
 // перечисление: элементы списка
-enum class Element {
-  CHERRY_PIE,
-  SECRET_BOX,
-  DRAGON_BALL,
-  GRAVITY_GUN,
-  BEAUTIFUL_FLOWERS,
-  UNINITIALIZED  // специальное значение, обозначающее отсутствие элемента
-};
+    enum class Element {
+        CHERRY_PIE,
+        SECRET_BOX,
+        DRAGON_BALL,
+        GRAVITY_GUN,
+        BEAUTIFUL_FLOWERS,
+        UNINITIALIZED  // специальное значение, обозначающее отсутствие элемента
+    };
 
 // внутренние проверки
-static_assert(static_cast<int>(Element::UNINITIALIZED) == 5, "Enum class Element contains too many values");
+    static_assert(static_cast<int>(Element::UNINITIALIZED) == 5, "Enum class Element contains too many values");
 
 }  // namespace itis
